3a3dd604aa7dbccd596f7a821fc5f84c18f71da8
Quentin Tennerel <tennerelquentin@gmail.com>
2026-05-26 17:32:03 +0800

    fix: update db version for tests

--- src/test_setup/db_setup.rs
+++ src/test_setup/db_setup.rs
@@ -4,7 +4,7 @@ use testcontainers::runners::AsyncRunner;
 use testcontainers::{ContainerAsync, GenericImage, ImageExt};
 use tokio_postgres::NoTls;
 
-static DB_VERSION: &str = "dev-44a6f2e";
+static DB_VERSION: &str = "dev-3bea6eb";
 
 pub struct TestDbConfig {
     pub host: String,

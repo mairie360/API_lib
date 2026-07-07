341dea5e01ec64240279a4fd6843d6f2d02fc680
Quentin Tennerel <tennerelquentin@gmail.com>
2026-07-07 19:20:27 +0200

    feat!: add first major version

--- src/test_setup/db_setup.rs
+++ src/test_setup/db_setup.rs
@@ -4,7 +4,7 @@ use testcontainers::runners::AsyncRunner;
 use testcontainers::{ContainerAsync, GenericImage, ImageExt};
 use tokio_postgres::NoTls;
 
-static DB_VERSION: &str = "0.3.0";
+static DB_VERSION: &str = "1.0.0";
 
 pub struct TestDbConfig {
     pub host: String,
